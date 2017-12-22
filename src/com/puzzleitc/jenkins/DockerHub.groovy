e6307d0e47c570a54f92ff6c6b142496f767d660
Christoph Raaflaub <raaflaub@puzzle.ch>
2017-12-22 14:13:29 +0100

    file formatting

--- src/com/puzzleitc/jenkins/DockerHub.groovy
+++ src/com/puzzleitc/jenkins/DockerHub.groovy
@@ -7,45 +7,39 @@ import groovy.json.JsonOutput
  * Docker Hub API utility class.
  */
 class DockerHub {
-  static String baseUrl = "https://hub.docker.com/v2/"
-  static String loginUrl = baseUrl + "users/login/"
+    static String BASE_URL = "https://hub.docker.com/v2/"
+    static String LOGIN_URL = BASE_URL + "users/login/"
 
-  /**
-   * Does a login into the Docker Hub API and returns the resulting token.
-   *
-   * @return the Docker Hub API login token
-   */
-  @com.cloudbees.groovy.cps.NonCPS
-  static String createLoginToken(String dockerHubUser, String dockerHubPwd) {
-    def auth = JsonOutput.toJson([username: dockerHubUser, password: dockerHubPwd])
+    /**
+     * Does a login into the Docker Hub API and returns the resulting token.
+     * Returning null means no successful login.
+     *
+     * @return the Docker Hub API login token
+     */
+    @com.cloudbees.groovy.cps.NonCPS
+    static String createLoginToken(String dockerHubUser, String dockerHubPwd) {
+        String auth = JsonOutput.toJson([username: dockerHubUser, password: dockerHubPwd])
 
-    def URL loginUrlObject = new URL(DockerHub.loginUrl);
+        URL loginUrl = new URL(DockerHub.LOGIN_URL);
+        HttpURLConnection con = (HttpURLConnection) loginUrl.openConnection()
+        con.setDoOutput(true)
+        con.setDoInput(true)
+        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8")
+        con.setRequestProperty("Accept", "application/json")
+        con.setRequestMethod("POST")
 
-    HttpURLConnection con = (HttpURLConnection) loginUrlObject.openConnection();
-    con.setDoOutput(true);
-    con.setDoInput(true);
-    con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-    con.setRequestProperty("Accept", "application/json");
-    con.setRequestMethod("POST");
+        OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream())
+        wr.write(auth.toString())
+        wr.flush()
 
-    OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
-    wr.write(auth.toString());
-    wr.flush();
-
-    //display what returns the POST request
-
-    StringBuilder sb = new StringBuilder();
-    int HttpResult = con.getResponseCode();
-
-    println "ResponseCode: " + HttpResult
-    println "ResponseMessage: " + con.getResponseMessage()
-
-    if (HttpResult == HttpURLConnection.HTTP_OK) {
-       def responseContent = new JsonSlurper().parseText(con.content.text)
-       return responseContent.token
-
-    } else {
-       return null
+        int httpResult = con.getResponseCode()
+        if (httpResult == HttpURLConnection.HTTP_OK) {
+            def responseContent = new JsonSlurper().parseText(con.content.text)
+            return responseContent.token
+        } else {
+            println "HTTP response code: " + httpResult
+            println "HTTP response message: " + con.getResponseMessage()
+            return null
+        }
     }
-  }
 }
