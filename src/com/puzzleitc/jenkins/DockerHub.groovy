c3724f7937d5f3e604f371b7446770207f8f49fd
Christoph Raaflaub <raaflaub@puzzle.ch>
2017-12-27 11:18:25 +0100

    tag deletion; echo support

--- src/com/puzzleitc/jenkins/DockerHub.groovy
+++ src/com/puzzleitc/jenkins/DockerHub.groovy
@@ -11,6 +11,17 @@ class DockerHub {
     static String LOGIN_URL = BASE_URL + "users/login/"
     static String REPOSITORIES_URL = BASE_URL + "repositories/"
 
+    private final script
+
+    /**
+     * This constructor sets the Jenkins 'script' class as the local script
+     * variable in order to resolve execution steps (sh, withCredentials, etc).
+     * @param script the script object
+     */
+    DockerHub(script) {
+        this.script = script
+    }
+
     /**
      * Does a login into the Docker Hub API and returns the resulting token.
      * Returning null means no successful login.
@@ -18,10 +29,10 @@ class DockerHub {
      * @return the Docker Hub API login token
      */
     @com.cloudbees.groovy.cps.NonCPS
-    static String createLoginToken(String dockerHubUser, String dockerHubPwd) {
+    String createLoginToken(String dockerHubUser, String dockerHubPwd) {
         String auth = JsonOutput.toJson([username: dockerHubUser, password: dockerHubPwd])
 
-        URL loginUrl = new URL(DockerHub.LOGIN_URL);
+        URL loginUrl = new URL(DockerHub.LOGIN_URL)
         HttpURLConnection con = (HttpURLConnection) loginUrl.openConnection()
         con.setDoOutput(true)
         con.setDoInput(true)
@@ -38,8 +49,8 @@ class DockerHub {
             def responseContent = new JsonSlurper().parseText(con.content.text)
             return responseContent.token
         } else {
-            println "HTTP response code: " + httpResult
-            println "HTTP response message: " + con.getResponseMessage()
+            script.echo "HTTP response code: " + httpResult
+            script.echo "HTTP response message: " + con.getResponseMessage()
             return null
         }
     }
@@ -51,30 +62,30 @@ class DockerHub {
      * @return a list of all repositories of the dockerHubUser.
      */
     @com.cloudbees.groovy.cps.NonCPS
-    static String readRepositoryNames(String dockerHubUser, String token) {
+    String readRepositoryNames(String dockerHubUser, String token) {
         def url = REPOSITORIES_URL + dockerHubUser
-        URL repositoriesUrl = new URL(url);
+        URL repositoriesUrl = new URL(url)
 
-        HttpURLConnection con = (HttpURLConnection) repositoriesUrl.openConnection();
-        con.setDoOutput(true);
-        con.setDoInput(true);
-        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-        con.setRequestProperty("Accept", "application/json");
-        con.setRequestProperty("Authorization", "JWT ${token}");
+        HttpURLConnection con = (HttpURLConnection) repositoriesUrl.openConnection()
+        con.setDoOutput(true)
+        con.setDoInput(true)
+        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8")
+        con.setRequestProperty("Accept", "application/json")
+        con.setRequestProperty("Authorization", "JWT ${token}")
 
         int httpResult = con.getResponseCode()
         if (httpResult == HttpURLConnection.HTTP_OK) {
             def responseContent = new JsonSlurper().parseText(con.content.text)
             def size = responseContent.count
             List repositoryNames = new ArrayList(size)
-            List repositoriesList = (List) responseContent.get("results");
+            List repositoriesList = (List) responseContent.get("results")
             for (int i = 0; i < size; i++) {
                 repositoryNames.add(repositoriesList[i].name)
             }
             return repositoryNames
         } else {
-            println "HTTP response code: " + httpResult
-            println "HTTP response message: " + con.getResponseMessage()
+            script.echo "HTTP response code: " + httpResult
+            script.echo "HTTP response message: " + con.getResponseMessage()
             return null
         }
     }
@@ -86,31 +97,62 @@ class DockerHub {
      * @return a list of all tags of the repository.
      */
     @com.cloudbees.groovy.cps.NonCPS
-    static String readTags(String dockerHubUser, String repository, String token) {
+    List readTags(String dockerHubUser, String repository, String token) {
         def url = REPOSITORIES_URL + dockerHubUser + "/" + repository + "/tags"
-        URL tagsUrl = new URL(url);
+        URL tagsUrl = new URL(url)
 
-        HttpURLConnection con = (HttpURLConnection) tagsUrl.openConnection();
-        con.setDoOutput(true);
-        con.setDoInput(true);
-        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-        con.setRequestProperty("Accept", "application/json");
-        con.setRequestProperty("Authorization", "JWT ${token}");
+        HttpURLConnection con = (HttpURLConnection) tagsUrl.openConnection()
+        con.setDoOutput(true)
+        con.setDoInput(true)
+        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8")
+        con.setRequestProperty("Accept", "application/json")
+        con.setRequestProperty("Authorization", "JWT ${token}")
 
         int httpResult = con.getResponseCode()
         if (httpResult == HttpURLConnection.HTTP_OK) {
             def responseContent = new JsonSlurper().parseText(con.content.text)
             def size = responseContent.count
             List tagNames = new ArrayList(size)
-            List tagsList = (List) responseContent.get("results");
+            List tagsList = (List) responseContent.get("results")
             for (int i = 0; i < size; i++) {
                 tagNames.add(tagsList[i].name)
             }
             return tagNames
         } else {
-            println "HTTP response code: " + httpResult
-            println "HTTP response message: " + con.getResponseMessage()
+            script.echo "HTTP response code: " + httpResult
+            script.echo "HTTP response message: " + con.getResponseMessage()
             return null
+        }
+    }
+
+    /**
+     * Reads all tags of the given repository and returns the names as list.
+     * Returning null means no successful reading.
+     *
+     * @return a list of all tags of the repository.
+     */
+    @com.cloudbees.groovy.cps.NonCPS
+    void deleteByTag(String dockerHubUser, String repository, String tagName, String token) {
+        def url = REPOSITORIES_URL + dockerHubUser + "/" + repository + "/tags/" + tagName
+        URL tagsUrl = new URL(url)
+
+        HttpURLConnection con = (HttpURLConnection) tagsUrl.openConnection()
+        con.setDoOutput(true)
+        con.setDoInput(true)
+        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8")
+        con.setRequestProperty("Accept", "application/json")
+        con.setRequestProperty("Authorization", "JWT ${token}")
+        con.setRequestMethod("DELETE")
+
+        int httpResult = con.getResponseCode()
+        if (httpResult == HttpURLConnection.HTTP_OK) {
+            def responseContent = new JsonSlurper().parseText(con.content.text)
+
+            script.echo "deleted"
+            script.echo responseContent
+        } else {
+            script.echo "HTTP response code: " + httpResult
+            script.echo "HTTP response message: " + con.getResponseMessage()
         }
     }
 }
