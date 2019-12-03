6dd64f0ce5ee4fb3f1e55d75ce712a3f215f2b47
Reto Galante <galante@puzzle.ch>
2019-12-03 09:15:06 +0100

    Integrated sources from GitLab

---
+++ src/com/puzzleitc/OpenShift.groovy
@@ -0,0 +1,33 @@
+package com.puzzleitc;
+
+def tar(openShiftConfigDir, String... deployables) {
+  String potHome = tool("pot")
+  sh "${potHome}/bin/pot tar ${openShiftConfigDir} ${deployables.join(' ')}"
+  archive 'deployment.tar.gz'
+}
+
+def deploy(Map args) {
+   echo "Deploying build ${args['build']} to tier ${args['tier']}."
+   copyArtifact(args['build'], "deployment.tar.gz")
+
+   exportVars(args)
+
+   String potHome = tool("pot")
+   sh "${potHome}/bin/pot deploy ${args['tier']} ${args['options']}"
+}
+
+def exportVars(Map args) {
+   // Export variables to environment for Puzzle OpenShift Tool
+   for (arg in args) {
+     env["POT_" + arg.key.replaceAll(/(\B[A-Z])/, /_$1/).toUpperCase()] = arg.value
+   }
+}
+
+def copyArtifact(String build, String artifact) {
+   def matcher = build =~ '/job/([^/]+)/([0-9]+)'
+   def job = matcher[0][1]
+   def buildNumber = matcher[0][2]
+   matcher = null
+
+   sh "cp -f ${env.JENKINS_HOME}/jobs/${job}/builds/${buildNumber}/archive/${artifact} ."
+}
---
+++ src/com/puzzleitc/jenkins/AvailableJDKs.groovy
@@ -0,0 +1,34 @@
+package com.puzzleitc.jenkins
+
+
+import groovy.json.JsonSlurperClassic
+
+/**
+ * Jenkins JDK upstream information.
+ */
+class AvailableJDKs {
+
+  /**
+   * Reads the JDK file of Jenkins that provides the information of the available JDK's.
+   * JDK file location: JENKINS_HOME/updates/hudson.tools.JDKInstaller
+   *
+   * @return the information to the available JDK's as Map
+   */
+  @com.cloudbees.groovy.cps.NonCPS
+  static Map jdks() {
+    def inputFile = new File(System.getProperty("JENKINS_HOME") +"/updates/hudson.tools.JDKInstaller")
+    def json = new JsonSlurperClassic().parseText(inputFile.text)
+    Map jdks = new TreeMap()
+
+    for (Map data : json.data) {
+      Map release = data.releases[0]
+        for (Map file : release.files) {
+          if (file.filepath =~ /(?i)linux.*(x64|x86_64|amd64)/) {
+            def matcher = file.filepath =~ /\/jdk-([^-_]+)/
+            jdks[data.name] = ['url': file.filepath, 'version': matcher[0][1]]
+          }
+        }
+    }
+    return jdks;
+  }
+}
