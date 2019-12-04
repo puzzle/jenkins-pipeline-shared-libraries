bbecfa12825ae8d929d6e31387f18363d074fd7d
franbuehler <franbuehler@gmail.com>
2019-12-04 13:29:22 +0100

    Make scan directories configurable.

--- vars/owaspDependencyCheck.groovy
+++ vars/owaspDependencyCheck.groovy
@@ -4,8 +4,7 @@
 
 def call(Map args = [:], String... scanDirs) {
     args = [tool: "owasp-dependency-check-5.2.4"] << args
-    println(args.tool)
-    withEnv(["PATH+DC=${tool name: '5.2.4', type: 'dependency-check'}/bin"]) {                    
+    withEnv(["PATH+DC=${tool name: args.tool, type: 'dependency-check'}/bin"]) {                    
         sh 'mkdir -p data report'
         scanArgs = scanDirs.collect { "--scan '$it'"}.join(' ')
         sh "dependency-check.sh ${scanArgs} --format 'ALL' --project 'OWASP Dependency Check' --out report ${args.extraArgs}"
