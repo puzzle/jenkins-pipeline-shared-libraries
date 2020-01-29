2723447dd7535b85b71defe40459027486e8b863
Daniel Tschan <tschan@puzzle.ch>
2020-01-29 14:20:50 +0100

    Remove tool type to allow dependency-check installation via custom installer.

--- vars/owaspDependencyCheck.groovy
+++ vars/owaspDependencyCheck.groovy
@@ -1,6 +1,6 @@
 def call(Map args = [:], String... scanDirs) {
     args = [tool: "owasp-dependency-check-5.2.4"] << args
-    withEnv(["PATH+DC=${tool name: args.tool, type: 'dependency-check'}/bin"]) {                    
+    withEnv(["PATH+DC=${tool name: args.tool}/bin"]) {
         sh 'mkdir -p data report'
         scanArgs = scanDirs.collect { "--scan '$it'"}.join(' ')
         sh "dependency-check.sh ${scanArgs} --format 'ALL' --out report ${args.extraArgs}"
