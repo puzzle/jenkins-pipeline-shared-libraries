f657ff8a7333ee37b80db024a98b05ec789081b8
Dominic Brügger <bruegger@puzzle.ch>
2020-03-26 16:42:56 +0100

    Echo debug information

--- vars/kustomizeBuild.groovy
+++ vars/kustomizeBuild.groovy
@@ -1,5 +1,7 @@
 def call(String resourceFile) {
+    echo "-- start kustomize build --"
+    echo "resource file: $resourceFile"
     withEnv(["PATH+KUSTOMIZE_HOME=${tool 'kustomize'}/bin"]) {
-        sh 'kustomize build ${resourceFile}'
+        sh "kustomize build ${resourceFile}"
     }
 }
