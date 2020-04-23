8df918dfc87526b68b3e8cfbd93c88db49eed1da
Dominic Brügger <bruegger@puzzle.ch>
2020-04-23 11:14:07 +0200

    Try to resolve CPS method mismatch

--- src/com/puzzleitc/jenkins/command/KustomizeCommand.groovy
+++ src/com/puzzleitc/jenkins/command/KustomizeCommand.groovy
@@ -3,18 +3,17 @@ package com.puzzleitc.jenkins.command
 class KustomizeCommand {
 
     private final String resource;
-    private final String kustomizeHome;
     private final PipelineContext ctx;
 
     KustomizeCommand(String resource, PipelineContext ctx) {
         this.ctx = ctx
         this.resource = resource
-        this.kustomizeHome = ctx.toolHome("kustomize")
     }
 
     Object execute() {
         ctx.log("-- start kustomize build --")
         ctx.log("resource: $resource")
+        def kustomizeHome = ctx.toolHome("kustomize")
         ctx.withEnvironment(["PATH+KUSTOMIZE_HOME=${kustomizeHome}/bin"]) {
             return ctx.shell(script: "kustomize build ${resource}", returnStdout: true)
         }
