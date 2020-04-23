555283aa81456149af6ece2bf6228f02ff27063f
Dominic Brügger <bruegger@puzzle.ch>
2020-04-23 16:27:54 +0200

    Add named parameters

--- vars/openshiftApply.groovy
+++ vars/openshiftApply.groovy
@@ -1,7 +1,7 @@
 import com.puzzleitc.jenkins.command.OpenshiftApplyCommand
 import com.puzzleitc.jenkins.command.context.JenkinsPipelineContext
 
-def call(String name) {
+def call(Map params = [:]) {
     OpenshiftApplyCommand command = new OpenshiftApplyCommand(new JenkinsPipelineContext())
     command.execute()
 }
