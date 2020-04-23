b30de5f850088988a1a8ec9fe83d720dfef1db95
Dominic Brügger <bruegger@puzzle.ch>
2020-04-23 08:22:47 +0200

    Return stdout

--- vars/kustomizeBuild.groovy
+++ vars/kustomizeBuild.groovy
@@ -2,6 +2,7 @@ def call(String resource) {
     echo "-- start kustomize build --"
     echo "resource: $resource"
     withEnv(["PATH+KUSTOMIZE_HOME=${tool 'kustomize'}/bin"]) {
-        sh("kustomize build ${resource}")
+        stdout = sh(script: "kustomize build ${resource}", returnStdout: true)
+        echo "Output: ${stdout}";
     }
 }
