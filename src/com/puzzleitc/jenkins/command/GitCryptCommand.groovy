c7173b94a2ce409c6d21ed4a7530b9f436012e76
Daniel Tschan <tschan@puzzle.ch>
2020-08-17 13:56:33 +0200

    Move all logic to command class. Add Util class with parseArgs function.

--- src/com/puzzleitc/jenkins/command/GitCryptCommand.groovy
+++ src/com/puzzleitc/jenkins/command/GitCryptCommand.groovy
@@ -5,26 +5,30 @@ import com.puzzleitc.jenkins.command.context.PipelineContext
 class GitCryptCommand {
 
     private final PipelineContext ctx
-    private String credentialsId
+    private boolean unlocked
 
     GitCryptCommand(PipelineContext ctx) {
         this.ctx = ctx
     }
 
     void execute() {
-        credentialsId = ctx.stepParams.getOptional('credentialsId', null) as String
+        def credentialsId = ctx.stepParams.getRequired('credentialsId') as String
+        def body = ctx.stepParams.getRequired('body') as Closure
+        def unlocked = false
         if (credentialsId) {
             ctx.info("-- git-crypt unlock --")
-            ctx.withCredentials([ctx.file(credentialsId: credentialsId, variable: 'GIT_CRYPT_KEYFILE')]) {
-                ctx.sh script: 'git-crypt unlock $GIT_CRYPT_KEYFILE'
+            try {
+                ctx.withCredentials([ctx.file(credentialsId: credentialsId, variable: 'GIT_CRYPT_KEYFILE')]) {
+                    ctx.sh script: 'git-crypt unlock $GIT_CRYPT_KEYFILE'
+                    unlocked = true
+                }
+                body()
+            } finally {
+                if (unlocked) {
+                    ctx.info("-- git-crypt lock --")
+                    ctx.sh script: 'git-crypt lock'
+                }
             }
-        }
-    }
-
-    void cleanUp() {
-        if (credentialsId) {
-            ctx.info("-- git-crypt lock --")
-            ctx.sh script: 'git-crypt lock'
         }
     }
 }
--- vars/withGitCrypt.groovy
+++ vars/withGitCrypt.groovy
@@ -1,13 +1,9 @@
 import com.puzzleitc.jenkins.command.GitCryptCommand
 import com.puzzleitc.jenkins.command.context.JenkinsPipelineContext
+import static com.puzzleitc.jenkins.Util.parseArgs
 
-def call(Map params = [:], body) {
-
-    GitCryptCommand command = new GitCryptCommand(new JenkinsPipelineContext(params))
-    try {
-        command.execute()
-        body()
-    } finally {
-        command.cleanUp()
-    }
+def call(Map namedArgs = [:], Object... positionalArgs) {
+    def args = parseArgs(namedArgs, positionalArgs, ["credentialsId", "body"])
+    GitCryptCommand command = new GitCryptCommand(new JenkinsPipelineContext(args))
+    command.execute()
 }
