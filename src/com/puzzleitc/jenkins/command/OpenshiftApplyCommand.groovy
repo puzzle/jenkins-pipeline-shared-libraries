ab1fc5564119ad9221eee4231d4cc66cc1c29caf
Dominic Brügger <bruegger@puzzle.ch>
2020-09-24 13:12:49 +0200

    Debug lookupSaToken

--- src/com/puzzleitc/jenkins/command/OpenshiftApplyCommand.groovy
+++ src/com/puzzleitc/jenkins/command/OpenshiftApplyCommand.groovy
@@ -49,7 +49,7 @@ class OpenshiftApplyCommand {
     private String lookupSaToken(String credentialId, project) {
         if (credentialId == null) {
             // Token is only needed when not running on Kubernetes cluster
-            ctx.echo("KUBERNETES_PORT: ${ctx.lookupEnvironmentVariable('KUBERNETES_PORT')}")
+            ctx.echo("KUBERNETES_PORT: ${System.getenv('KUBERNETES_PORT')}")
             if (System.getenv('KUBERNETES_PORT') == null) {
                 ctx.lookupTokenFromCredentials("${project}${DEFAULT_CREDENTIAL_ID_SUFFIX}")
             } else {
