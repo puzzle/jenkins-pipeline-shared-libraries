7c1369f3297fe09ba20b3808d162bcc40cd015b0
Dominic Brügger <bruegger@puzzle.ch>
2020-10-19 15:50:31 +0200

    Added more ugly code

--- src/com/puzzleitc/jenkins/command/context/StepMetrics.groovy
+++ src/com/puzzleitc/jenkins/command/context/StepMetrics.groovy
@@ -10,17 +10,33 @@ class StepMetrics {
     private final JenkinsInvoker invoker = new JenkinsInvoker()
 
     void increment() {
-        invoker.callEcho('Gugus')
 
+        Counter counter = findCounter('shared_library_step_executions_total')
+        counter.inc()
+
+        invoker.callEcho('Successfully incremented counter')
+    }
+
+    private Counter findOrCreateCounter(String name) {
+        Counter counter = findCounter(name)
+        if (counter == null) {
+            return Counter.build().name(name).register()
+        } else {
+            return counter
+        }
+    }
+
+    private Counter findCounter(String name) {
         Set<Collector> collectors = CollectorRegistry.defaultRegistry.collectors()
         for (Collector collector : collectors) {
-            if (collector instanceof Collector.Describable) {
-                List<Collector.MetricFamilySamples> mfs = ((Collector.Describable) collector).describe()
-                for (Collector.MetricFamilySamples family : mfs) {
-                    invoker.callEcho("Collector: ${family.name}")
+            if (collector instanceof Counter) {
+                List<Collector.MetricFamilySamples> mfs = ((Counter) collector).describe()
+                if (mfs.size() == 1 && name.equals(mfs.iterator().next().name)) {
+                    return (Counter) collector
                 }
             }
         }
+        return null
     }
 
 }
