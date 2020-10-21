b0bf8d8d0bbb72a6bfc52bcb1db1c2a16736701f
Dominic Brügger <bruegger@puzzle.ch>
2020-10-21 10:11:44 +0200

    Improve logging

--- src/com/puzzleitc/jenkins/command/OwaspDependencyCheckCommand.groovy
+++ src/com/puzzleitc/jenkins/command/OwaspDependencyCheckCommand.groovy
@@ -17,17 +17,20 @@ class OwaspDependencyCheckCommand {
         ctx.info('-- owaspDependencyCheck --')
         def dcTool = ctx.stepParams.getOptional('tool', DEFAULT_DC_TOOL_NAME) as String
         def dcHome = ctx.tool(dcTool)
+        def dcArgs = createDependencyCheckArgs(ctx.stepParams)
+        ctx.echo("dependency-check tool: $dcTool")
+        ctx.echo("dependency-check arguments: $dcArgs")
         ctx.withEnv(["PATH+DC_HOME=${dcHome}/bin"]) {
             ctx.sh(script: 'mkdir -p data report')
-            ctx.sh(script: "dependency-check.sh ${createDependencyCheckArgs(ctx.stepParams)}")
+            ctx.sh(script: "dependency-check.sh $dcArgs")
         }
         ctx.dependencyCheckPublisher(pattern: 'report/dependency-check-report.xml')
     }
 
     private String createDependencyCheckArgs(StepParams stepParams) {
-        def scanArgs = stepParams.getRequired('scanDirs').collect { "--scan '$it'" }.join(' ')
-        def args = "${scanArgs} --format 'ALL' --out report ${stepParams.getOptional('extraArgs', '')}"
-        ctx.echo("DependencyCheckArgs: $args")
-        return args
+        def result = stepParams.getRequired('scanDirs').collect { "--scan '$it'" }.join(' ')
+        result << ' --format ALL --out report'
+        result << " ${stepParams.getOptional('extraArgs', '')}"
+        return result
     }
 }
