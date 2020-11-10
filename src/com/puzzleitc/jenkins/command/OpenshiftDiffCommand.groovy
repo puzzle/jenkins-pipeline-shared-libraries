d0f26f3fe1359075fcd7f60910fef0243ae41323
Daniel Tschan <tschan@puzzle.ch>
2020-11-10 16:30:05 +0100

    Install oc in openshiftDiff step

--- src/com/puzzleitc/jenkins/command/OpenshiftDiffCommand.groovy
+++ src/com/puzzleitc/jenkins/command/OpenshiftDiffCommand.groovy
@@ -4,7 +4,6 @@ import com.puzzleitc.jenkins.command.context.PipelineContext
 
 class OpenshiftDiffCommand {
 
-    private static final DEFAULT_OPENSHIFT_DIFF_EXECUTABLE = 'openshift-diff'
     private static final DEFAULT_OPENSHIFT_DIFF_TOOL = 'openshift_diff'
 
     private final PipelineContext ctx
@@ -18,16 +17,19 @@ class OpenshiftDiffCommand {
         def configuration = ctx.stepParams.getRequired('configuration') as String
         def project = ctx.stepParams.getRequired('project')
         def cluster = ctx.stepParams.getOptional('cluster')
-        def openshiftDiffPath = ctx.executable(DEFAULT_OPENSHIFT_DIFF_EXECUTABLE, DEFAULT_OPENSHIFT_DIFF_TOOL)
+        def ocPath = ctx.executable('oc')
+        def openshiftDiffPath = ctx.executable('openshift-diff', DEFAULT_OPENSHIFT_DIFF_TOOL)
         def credentialsId = ctx.stepParams.getOptional('credentialsId', null) as String
         def saToken = ctx.lookupServiceAccountToken(credentialsId, project)
-        ctx.openshift.withCluster(cluster) {
-            ctx.openshift.withProject(project) {
-                ctx.openshift.withCredentials(saToken) {
-                    ctx.echo("openshift whoami: ${ctx.openshift.raw('whoami').out.trim()}")
-                    ctx.echo("openshift cluster: ${ctx.openshift.cluster()}")
-                    ctx.echo("openshift project: ${ctx.openshift.project()}")
-                    ctx.sh(script: "${openshiftDiffPath}/openshift-diff -n ${project} <<< '${configuration}'")
+        ctx.withEnv(["PATH+OPENSHIFT_DIFF=${ocPath}:${openshiftDiffPath}"]) {
+            ctx.openshift.withCluster(cluster) {
+                ctx.openshift.withProject(project) {
+                    ctx.openshift.withCredentials(saToken) {
+                        ctx.echo("openshift whoami: ${ctx.openshift.raw('whoami').out.trim()}")
+                        ctx.echo("openshift cluster: ${ctx.openshift.cluster()}")
+                        ctx.echo("openshift project: ${ctx.openshift.project()}")
+                        ctx.sh(script: "openshift-diff -n ${project} <<< '${configuration}'")
+                    }
                 }
             }
         }
