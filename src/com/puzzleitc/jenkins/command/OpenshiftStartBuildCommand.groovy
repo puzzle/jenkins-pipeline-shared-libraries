1793561a349f5bd2aa96df3ccae52e1b61d5844e
Christoph Raaflaub <raaflaub@puzzle.ch>
2021-07-12 10:21:06 +0200

    OpenshiftStartBuild check for oc installed

--- src/com/puzzleitc/jenkins/command/OpenshiftStartBuildCommand.groovy
+++ src/com/puzzleitc/jenkins/command/OpenshiftStartBuildCommand.groovy
@@ -2,8 +2,6 @@ package com.puzzleitc.jenkins.command
 
 import com.puzzleitc.jenkins.command.context.PipelineContext
 
-import static com.puzzleitc.jenkins.command.Constants.DEFAULT_OC_TOOL_NAME
-
 class OpenshiftStartBuildCommand {
 
     private final PipelineContext ctx
@@ -21,20 +19,19 @@ class OpenshiftStartBuildCommand {
         def fromFile = ctx.stepParams.getOptional('fromFile') as String
         def credentialsId = ctx.stepParams.getOptional('credentialsId') as String
         def saToken = ctx.lookupServiceAccountToken(credentialsId, project)
-        ctx.withEnv(["PATH+OC=${ctx.executable('oc', DEFAULT_OC_TOOL_NAME)}"]) {
-            ctx.openshift.withCluster(cluster) {
-                ctx.openshift.withProject(project) {
-                    ctx.openshift.withCredentials(saToken) {
-                        ctx.echo("openshift cluster: ${ctx.openshift.cluster()}")
-                        ctx.echo("openshift project: ${ctx.openshift.project()}")
-                        def bc = ctx.openshift.selector("bc/${buildConfigName}")
-                        def build = bc.startBuild(createStartBuildParams(fromDir, fromFile))
-                        def result = build.logs('-f')
-                        ctx.echo("oc startBuild action: ${result.actions[0].cmd}")
-                        ctx.echo("oc startBuild status: ${result.status}")
-                        if (build.object().status.phase == 'Failed') {
-                            ctx.fail("openshift build ${build.object().metadata.name} failed")
-                        }
+        ctx.ensureOcInstallation()
+        ctx.openshift.withCluster(cluster) {
+            ctx.openshift.withProject(project) {
+                ctx.openshift.withCredentials(saToken) {
+                    ctx.echo("openshift cluster: ${ctx.openshift.cluster()}")
+                    ctx.echo("openshift project: ${ctx.openshift.project()}")
+                    def bc = ctx.openshift.selector("bc/${buildConfigName}")
+                    def build = bc.startBuild(createStartBuildParams(fromDir, fromFile))
+                    def result = build.logs('-f')
+                    ctx.echo("oc startBuild action: ${result.actions[0].cmd}")
+                    ctx.echo("oc startBuild status: ${result.status}")
+                    if (build.object().status.phase == 'Failed') {
+                        ctx.fail("openshift build ${build.object().metadata.name} failed")
                     }
                 }
             }
