e7a022b86c04ce4f160234f679a027101bca75eb
Yannik Daellenbach <daellenbach@puzzle.ch>
2022-02-07 14:59:38 +0100

    add pipeline test for "mandatoryParameter"

---
+++ test/groovy/pipeline/CheckPipelineTest.groovy
@@ -0,0 +1,67 @@
+package groovy.pipeline
+
+import com.lesfurets.jenkins.unit.declarative.DeclarativePipelineTest
+import org.junit.Before
+import org.junit.Test
+
+import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
+import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
+import static com.lesfurets.jenkins.unit.global.lib.ProjectSource.projectSource
+import static org.assertj.core.api.Assertions.assertThat
+
+class CheckPipelineTest extends DeclarativePipelineTest {
+
+    @Before
+    @Override
+    void setUp() throws Exception {
+        super.setUp()
+        def library = library().name('jenkins-shared-library')
+                .defaultVersion('master')
+                .allowOverride(true)
+                .implicit(true)
+                .targetPath('<notNeeded>')
+                .retriever(projectSource())
+                .build()
+        helper.registerSharedLibrary(library)
+    }
+
+    @Test
+    void "mandatory parameter not set"() {
+        runInlineScript('''
+pipeline {
+    agent any
+    stages {
+        stage("Test") {
+            check.mandatoryParameter("hallo")
+        }
+    }
+}
+    ''')
+
+        assertThat(helper.callStack.findAll { call ->
+            call.methodName == "error"
+        }.any { call ->
+            callArgsToString(call).contains("missing parameter: hallo")
+        }).isTrue()
+
+        printCallStack()
+    }
+
+    @Test
+    void "mandatory parameter set by default parameter"() {
+        runInlineScript('''
+pipeline {
+    agent any
+    parameters {
+        string(name: 'hallo', defaultValue: 'Mr Jenkins')
+    }
+    stages {
+        stage("Test") {
+            check.mandatoryParameter("hallo")
+        }
+    }
+}
+    ''')
+        assertJobStatusSuccess()
+    }
+}
