8402e5374d9d10a6fc00f3994548135e89be16ef
Christoph Raaflaub <raaflaub@puzzle.ch>
2022-02-07 09:52:00 +0100

    change from curl to http

--- src/com/puzzleitc/jenkins/command/KubeVersionCommand.groovy
+++ src/com/puzzleitc/jenkins/command/KubeVersionCommand.groovy
@@ -1,6 +1,7 @@
 package com.puzzleitc.jenkins.command
 
 import com.puzzleitc.jenkins.command.context.PipelineContext
+import groovy.json.JsonSlurper
 
 class KubeVersionCommand {
 
@@ -16,10 +17,48 @@ class KubeVersionCommand {
 
         server = (server != null) ? server.replace('insecure://', 'https://') : 'https://kubernetes.default.svc'
 
-        def out = ctx.sh(script: "curl -sSk ${server}/version", returnStdout: true)
-        def version = readJSON text: out
+        def version = readVersion(server)
         ctx.echo("${server} version: ${version.major}.${version.minor}")
 
         return "${version.major}.${version.minor}"
+    }
+
+    Object readVersion(String server) {
+        def url = "${server}/version"
+        HttpURLConnection con
+        try {
+            con = connect(url)
+        } catch (Throwable t) {
+            printError('readVersion', t)
+        }
+        int httpResult = con.getResponseCode()
+        if (httpResult == HttpURLConnection.HTTP_OK) {
+            def version = new JsonSlurper().parseText(con.content.text)
+            return version
+        } else {
+            ctx.echo( "HTTP response code: " + httpResult)
+            ctx.echo( "HTTP response message: " + con.getResponseMessage())
+            ctx.fail("kubeVersion:readVersion failure, see previous HTTP messages")
+        }
+    }
+
+    private HttpURLConnection connect(String url) {
+        try {
+            URL targetURL = new URL(url)
+            HttpURLConnection con = (HttpURLConnection) targetURL.openConnection()
+            con.setDoOutput(true)
+            con.setDoInput(true)
+            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8")
+            con.setRequestProperty("Accept", "application/json")
+            return con
+        } catch (Throwable t) {
+            printError('connect', t)
+        }
+    }
+
+    private void printError(String functionName, Throwable t) {
+        ctx.echo("-- kubeVersion:${functionName} exception --")
+        ctx.echo(t.getMessage())
+        throw t
     }
 }
