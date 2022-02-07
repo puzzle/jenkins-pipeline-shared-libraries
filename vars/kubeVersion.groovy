2022c031afba6c7e9e493b41f090614d23be04d3
Christoph Raaflaub <raaflaub@puzzle.ch>
2022-02-07 09:44:20 +0100

    kubeVersion parse args

--- vars/kubeVersion.groovy
+++ vars/kubeVersion.groovy
@@ -4,7 +4,10 @@
 import com.puzzleitc.jenkins.command.KubeVersionCommand
 import com.puzzleitc.jenkins.command.context.JenkinsPipelineContext
 
-def call(Map args = [:]) {
+import static com.puzzleitc.jenkins.Util.parseArgs
+
+def call(Map namedArgs = [:], Object... positionalArgs) {
+  def args = parseArgs(namedArgs, positionalArgs, [], ['server': null])
   KubeVersionCommand command = new KubeVersionCommand(new JenkinsPipelineContext(this, args))
   return command.execute()
 }
