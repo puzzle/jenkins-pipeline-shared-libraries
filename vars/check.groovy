d956080af3f69a5d1e04f03a97b9c046ee73dc6f
SylivanKenobi <sypsilon@hotmail.com>
2023-10-13 14:38:13 +0200

    remove all methods who are deprecated or use bad plugins

--- vars/check.groovy
+++
@@ -1,7 +0,0 @@
-// Deprecated: Will be implemented as a step.
-def mandatoryParameter(parameterName) {
-    if (!params.containsKey(parameterName)) {
-        currentBuild.result = 'ABORTED'
-        error('missing parameter: ' + parameterName)
-    }
-}
