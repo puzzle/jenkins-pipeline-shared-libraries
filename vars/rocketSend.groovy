42bf06b17c7f1ab339d1cea5c87e4e9cae50acf0
RetGal <galante@puzzle.ch>
2025-01-09 15:36:10 +0100

    Refactor rocket send (#65) (#66)
    * refactor rocket send

--- vars/rocketSend.groovy
+++ vars/rocketSend.groovy
@@ -1,22 +1,12 @@
+import com.puzzleitc.jenkins.command.RocketSendCommand
+import com.puzzleitc.jenkins.command.context.JenkinsPipelineContext
+
 def void call(String webHook, String message, String avatar = null, Boolean rawMessage = true) {
-    Map data = [:] as Map
-    data['text'] = message
-    data['rawMessage'] = rawMessage
-
-    if (avatar != null) {
-        data['avatar'] = avatar
-    }
-
-    def curlCommand = 'curl ' +
-            '     -X POST "' + webHook + '" ' +
-            '     -H "Content-Type: application/json" ' +
-            '     --data \'' + groovy.json.JsonOutput.toJson(data) + '\' '
-
-    def returnCode = sh(script: curlCommand, returnStatus: true)
-
-    if (returnCode != 0) {
-        error("RocketChat notification failed!")
-    } else {
-        echo "RocketChat notification sent successfully"
-    }
+    RocketSendCommand command = new RocketSendCommand(new JenkinsPipelineContext(this, [
+            webHook: webHook,
+            message: message,
+            avatar: avatar,
+            rawMessage: rawMessage
+    ]))
+    command.execute()
 }
